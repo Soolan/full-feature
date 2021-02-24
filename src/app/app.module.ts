dabe64541327c55e44ae7eeaea1a0d0684f45edd
Soolan <info@soolan.com>
2021-02-24 10:38:53 +0800

    configs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NewComponent } from './new/new.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewComponent
   ],
   imports: [
     BrowserModule,
